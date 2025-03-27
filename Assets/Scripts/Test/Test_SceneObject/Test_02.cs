490754ee9f2e4b5aa16378d7a9dfa030d1b3d053
All3D4Y <dwlee95@gmail.com>
2025-03-28 06:55:08 +0900

    250328_애니메이션 기본
    -> 노트 생성, 판정

--- Assets/Scripts/Test/Test_SceneObject/Test_02.cs
+++ Assets/Scripts/Test/Test_SceneObject/Test_02.cs
@@ -11,31 +11,54 @@ public class Test_02 : TestBase
         animator = GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f)
+            ResetTransform();
+    }
+
     protected override void OnTest1(InputAction.CallbackContext context)
     {
         // 오른손 아래
+        ResetTransform();
         animator.SetFloat("AttackValue", 0.0f);
-        animator.SetTrigger("Attack");
+        animator.SetTrigger("RightAttack");
     }
 
     protected override void OnTest2(InputAction.CallbackContext context)
     {
         // 오른손 위
+        ResetTransform();
         animator.SetFloat("AttackValue", 1.0f);
-        animator.SetTrigger("Attack");
+        animator.SetTrigger("RightAttack");
     }
 
     protected override void OnTest3(InputAction.CallbackContext context)
     {
         // 왼손 아래
+        ResetTransform();
         animator.SetFloat("AttackValue", 0.0f);
-        animator.SetTrigger("Attack");
+        animator.SetTrigger("LeftAttack");
     }
 
     protected override void OnTest4(InputAction.CallbackContext context)
     {
         // 왼손 위
+        ResetTransform();
         animator.SetFloat("AttackValue", 1.0f);
-        animator.SetTrigger("Attack");
+        animator.SetTrigger("LeftAttack");
+    }
+
+    protected override void OnTest5(InputAction.CallbackContext context)
+    {
+        // 양손
+        ResetTransform();
+        animator.SetTrigger("BothAttack");
+    }
+
+    void ResetTransform()
+    {
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward);
     }
 }
