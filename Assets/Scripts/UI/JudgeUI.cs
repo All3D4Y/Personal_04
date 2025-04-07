31741ce89d95a529383beadb160b088ab00c5b3f
All3D4Y <dwlee95@gmail.com>
2025-04-07 16:37:39 +0900

    250407_판정UI

---
+++ Assets/Scripts/UI/JudgeUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JudgeUI : MonoBehaviour
+{
+    Image[] images;
+
+    void Awake()
+    {
+        images = new Image[transform.childCount];
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i] = transform.GetChild(i).GetComponent<Image>();
+        }
+    }
+
+    void Start()
+    {
+        foreach (Image image in images)
+        {
+            image.enabled = false;
+        }
+    }
+
+
+}
